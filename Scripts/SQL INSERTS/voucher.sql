127b8d6c3629e41400a46bcc9355198ea4c57615
msuribec <37965605+msuribec@users.noreply.github.com>
2019-06-03 22:42:10 -0500

    Add files via upload

---
+++ Scripts/SQL INSERTS/voucher.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM airline.voucher
+-- Date: 2019-06-03 22:36
+*/
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('12001A1','First class','A1',600,12001);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('12001A2','Executive','A2',605,12001);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('1A3','Tourist','A3',3,1);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('1A4','Tourist','A4',4,1);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('1A5','Tourist','A5',5,1);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('1A6','Tourist','A6',6,1);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('1A7','Tourist','A7',7,1);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('1A8','Tourist','A8',8,1);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('12003B3','Tourist','B3',606,12003);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('12006B5','First class','B5',603,12006);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('12004C1','First class','C1',601,12004);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('12002D2','Executive','D2',607,12002);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('12005D2','Tourist','D2',602,12005);
+INSERT INTO `` (`idVoucher`,`Class`,`Seat`,`Passenger_idPassenger`,`Flight_idFlight`) VALUES ('12002D7','First class','D7',604,12002);
